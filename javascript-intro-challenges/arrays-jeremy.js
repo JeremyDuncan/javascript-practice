611fe44abe30ff387064145925daed3d689477aa
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-03 17:51:51 -0400

    completed challenge 1

--- javascript-intro-challenges/arrays-jeremy.js
+++ javascript-intro-challenges/arrays-jeremy.js
@@ -2,11 +2,11 @@
 // Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
 
 // Consider the variable:
-// ```javascript
-// var groceryList = ["chips", "dip", "cookies"]
-// ```
+var groceryList = ["chips", "dip", "cookies"]
 
 // 1. Write the code that will add "soda" to the end of the original array.
+groceryList.push("soda");
+console.log(groceryList);
 
 // 2. Write the code that will add "granola" to the end of array without altering the original array.
 
