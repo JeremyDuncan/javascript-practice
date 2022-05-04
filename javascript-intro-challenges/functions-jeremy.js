482a4dfafd8221cdf52fabf63c37f54eb7e172ab
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-04 18:50:40 -0400

    completed more challenges.

--- javascript-intro-challenges/functions-jeremy.js
+++ javascript-intro-challenges/functions-jeremy.js
@@ -44,13 +44,37 @@
   multiply(5, 5);
 
 // 6. Write a function named `divisibleBy` that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-  function divisbleBy(num1, num2) {
-
+  function divisibleBy(num1, num2) {
+    if ( num1 % num2 === 0) {
+      return console.log(num1 + " is evenly divisible by " + num2);
+    } else {
+      return console.log(num1 + " is not evenly divisible by " + num2);
+    }
   }
+
+  divisibleBy(5, 5);
+  divisibleBy(5, 15);
+
 // 7. Write a function named `assignGrade` that takes a number score as an argument and returns the letter grade for the score.
   function assignGrade(score) {
-
+    if (score === 100) {
+      return console.log("perfect score");
+    } else if ( score < 100 && score >89) {
+      return console.log("score = A");
+    } else if ( score < 90 && score >79) {
+      return console.log("score = B");
+    } else if ( score < 80 && score >69) {
+      return console.log("score = C");
+    } else if ( score < 70 && score >59) {
+      return console.log("score = D");
+    } else if ( score < 60 &&  score != 0) {
+      return console.log("score = F");
+    } else {
+      return console.log("no score available");
+    }
   }
+
+  assignGrade(89);
 
 // 8. Write a function named `isLonger` that takes two strings as arguments and returns the string that contains the most characters.
   function isLonger(string1, string2) {
@@ -59,23 +83,52 @@
 
 // 9. Write a function named `greaterNum` that takes two numbers as arguments and returns whichever number is the greater (higher) number.
   function greaterNum(num1, num2) {
-
+    if (num1 > num2) {
+      return console.log(num1 + " is higher.")
+    } else {
+      return console.log(num2 + " is higher.")
+    }
   }
 
+  greaterNum(5, 6);
+  greaterNum(1000, 10);
+
 // 10. Write a function named `yelling` that takes a string as an argument and return the string in all uppercase case letters.
-  
+  function yelling(string) {
+    console.log(string.toUpperCase());
+  }
+
+  yelling("hello");
 
 // ### STRETCH Challenges
 
 // 1. The World Translator   
   // (a) Write a function named `helloWorld` that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.  
   // (b) Have your function [default](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters) to returning English.
+  function helloWorld(languageCode) {
+    if (languageCode === "en") {
+      console.log("Hello World!")
+    } else if (languageCode === " ## ") {
+      console.log(" ### ")
+    } else if (languageCode === " ## ") {
+      console.log(" ### ")
+    }  else if (languageCode === " ## ") {
+      console.log(" ### ")
+    } else if (languageCode === " ## ") {
+      console.log(" ### ")
+    }
+  }
+
+  helloWorld("en");
+  helloWorld(" ");
 
 // 2. The Pluralizer  
  //  (a) Write a function named `pluralizer` that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+  function pluralizer(number, noun) {
+    
+  }
 
-  //```javascript
-  //pluralizer(5, "cat")
+  pluralizer(5, "cat")
   // expected output: "5 cats"
 
   //pluralizer(1, "dog")
