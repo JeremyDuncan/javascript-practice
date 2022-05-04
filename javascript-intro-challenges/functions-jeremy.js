114f0ca7173d3e24e76a540e0a8d061c6706eed5
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-04 14:45:47 -0400

    Created functions challenge.

---
+++ javascript-intro-challenges/functions-jeremy.js
@@ -0,0 +1,42 @@
+// ### Challenges
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt. Each solution requires pseudocode. Test your functions with multiple function calls.
+
+// 1. Write a function named `marco` that returns "polo".
+
+// 2. Write a function named `greeting` that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// 3. Write a function named `oddOrEven` that takes a number as an argument and returns whether the number is odd or even.
+
+// 4. Write a function named `triple` that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// 5. Write a function named `multiply` that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+// 6. Write a function named `divisibleBy` that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// 7. Write a function named `assignGrade` that takes a number score as an argument and returns the letter grade for the score.
+
+// 8. Write a function named `isLonger` that takes two strings as arguments and returns the string that contains the most characters.
+
+// 9. Write a function named `greaterNum` that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// 10. Write a function named `yelling` that takes a string as an argument and return the string in all uppercase case letters.
+
+
+### STRETCH Challenges
+
+// 1. The World Translator   
+  // (a) Write a function named `helloWorld` that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.  
+  // (b) Have your function [default](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters) to returning English.
+
+// 2. The Pluralizer  
+ //  (a) Write a function named `pluralizer` that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+
+  ```javascript
+  pluralizer(5, "cat")
+  // expected output: "5 cats"
+
+  pluralizer(1, "dog")
+  // expected output: "1 dog"
+  ```
+
+  (b) Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species".
