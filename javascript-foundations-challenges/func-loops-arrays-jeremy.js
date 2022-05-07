47c58ba21c0d8e56e794395b057b9bdedaf19d47
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-06 20:54:56 -0400

    Completed the first 2 challenges.

--- javascript-foundations-challenges/func-loops-arrays-jeremy.js
+++ javascript-foundations-challenges/func-loops-arrays-jeremy.js
@@ -3,55 +3,64 @@
 // **Don't forget to pseudo code.**
 
 // 1. Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
+var multiplyByThree = (num) => {
+  var result = [];
+  for (var i = 0; i < num.length; i++) {
+    result.push(num[i] * 3);
+  }
+  return result;
+}
 
-```javascript
-var testArr1 = [3, 9, 15, 4, 10]
+var testArr1 = [3, 9, 15, 4, 10];
+var newArr = multiplyByThree(testArr1);
+console.log(newArr);
 // --> [9, 27, 45, 12, 30]
-```
 
 // 2. Write a function that takes in an array of numbers and returns a new array with only odd numbers.
+function getOdd(num) {
+  var result = [];
+  
+  for (var i = 0; i < num.length; ++i) {
+    if (num[i] % 2 != 0) {
+      result.push(num[i]);
+    }
+  }
+  return result;
+}
 
-```javascript
 var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+var challenge2 = getOdd(testArr2);
+console.log(challenge2);
 // --> [-7, 3, 5, 13]
-```
 
 // 3. Write a function that takes in an array of numbers and letters and returns a string with only the letters. **HINT:** use the `typeof` method.
-
-```javascript
 var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"]
 // --> "nicework"
-```
 
 // 4. Create a function that takes in an array of numbers and returns the sum.
 
-```javascript
 var addThese1 = [1, 2, 3, 4]
 // --> 10
 
 var addThese2 = []
 // --> 0
-```
+
 
 // 5. Create a function that takes in an array of numbers and returns the index of the largest number.
 
-```javascript
 var indexHighestNumber = [1, 4, 2, 3]
 // --> 1
-```
+
 
 // **STRETCH Challenges**
 // 1. Create a function that takes in two arrays and returns one array with no duplicate values.
 
-```javascript
+
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // --> [3, 7, 10, 5, 4, 8, 2, 1]
-```
 
 // 2. Create a function that takes in two numbers as arguments and returns an array the length of the first number filled with the second number.
-
-```javascript
 var arrayLength = 6
 var arrayValue = 0
 // --> [0, 0, 0, 0, 0, 0]
@@ -59,11 +68,10 @@ var arrayValue = 0
 var arrayLength = 4
 var arrayValue = 11
 // --> [11, 11, 11, 11]
-```
+
 
 // 3. Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function.
 
-```javascript
 var addUp1 = 4
 // 1 + 2 + 3 + 4 = 10
 // --> 10
@@ -74,7 +82,7 @@ var addUp2 = 10
 
 var addUp3 = 600
 // --> 180300
-```
+
 
 // **EPIC Challenges**
 
