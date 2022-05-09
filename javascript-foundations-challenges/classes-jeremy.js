da006c0b9e0a38b9a99af8a362ded695c3c913e7
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-09 19:24:50 -0400

    Completed 2 challenges

--- javascript-foundations-challenges/classes-jeremy.js
+++ javascript-foundations-challenges/classes-jeremy.js
@@ -1,53 +1,73 @@
-## Challenges
+// ## Challenges
 
-1. Coffee Maker
+// 1. Coffee Maker
+  class Coffee {
+    constructor(type, cream, sugar){
+      this.type = type.toLowerCase()
+      this.cream = cream
+      this.sugar = sugar
+    }
 
-```javascript
-class Coffee {
-  constructor(type, cream, sugar){
-    this.type = type.toLowerCase()
-    this.cream = cream
-    this.sugar = sugar
-  }
+    coffeeProfile(){
+      return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+    }
 
-  coffeeProfile(){
-    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
-  }
+    creams(){
+      if(this.cream > 1){
+        return `${this.cream} creams`
+      } else {
+        return `${this.cream} cream`
+      }
+    }
 
-  creams(){
-    if(this.cream > 1){
-      return `${this.cream} creams`
-    } else {
-      return `${this.cream} cream`
+    sugars(){
+      if(this.sugar > 1){
+        return `${this.sugar} sugars`
+      } else {
+        return `${this.sugar} sugar`
+      }
     }
   }
 
-  sugars(){
-    if(this.sugar > 1){
-      return `${this.sugar} sugars`
-    } else {
-      return `${this.sugar} sugar`
+// - Write the code that makes a black coffee. Then write the code that outputs the coffee's profile.
+  var blackCoffee = new Coffee("black", 0, 0);
+  console.log(blackCoffee);
+
+// - Write the code that makes a coffee with 1 cream and 2 sugars. Then write the code that outputs the coffee's profile.
+  var momsCoffee = new Coffee("Mom\'s Coffee", 1, 2);
+  console.log(momsCoffee);
+
+// 2. Latte Maker
+// - Write a Latte class that takes a flavor, a milk type and a number of shots.
+// - Write a method for your Latte class that outputs the latte's profile.
+  class Latte {
+    constructor(flavor, milk, shots){
+      this.flavor = flavor.toLowerCase()
+      this.milk = milk.toLowerCase()
+      this.shots = shots
+    }
+    shot() {
+      if (this.shots > 1) {
+        return `${this.shots} shots`;
+      } else {
+        return `${this.shots} shot`;
+      }
+    }
+    describe() {
+      console.log(`A ${this.flavor} latte with ${this.milk} milk and ${this.shot()} of expresso.`);
     }
   }
-}
-```
 
-- Write the code that makes a black coffee. Then write the code that outputs the coffee's profile.
+// - Write the code that makes a regular, single shot latte. Then, log the latte's profile.
+  var regularLatte = new Latte("regular", "whole", 1);
+  regularLatte.describe();
 
-- Write the code that makes a coffee with 1 cream and 2 sugars. Then write the code that outputs the coffee's profile.
+// - Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
+  var hazelNutLatte = new Latte("hazelnut", "almond", 2);
+  hazelNutLatte.describe();
 
-2. Latte Maker
+// 3. Volume of a Cylinder
 
-- Write a Latte class that takes a flavor, a milk type and a number of shots.
+// - Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr<sup>2</sup>h (r is the radius and h is the height of the cylinder)
 
-- Write a method for your Latte class that outputs the latte's profile.
-
-- Write the code that makes a regular, single shot latte. Then, log the latte's profile.
-
-- Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
-
-3. Volume of a Cylinder
-
-- Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr<sup>2</sup>h (r is the radius and h is the height of the cylinder)
-
-- Write the code that creates three unique cylinder objects
+// - Write the code that creates three unique cylinder objects
