b5d598b5ca6f2f52d2ce4ed6dfd1e2c73c95585a
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-10 14:11:06 -0400

    Added comments to my code

--- react-challenges/state-jeremy/src/components/Colorbox.js
+++ react-challenges/state-jeremy/src/components/Colorbox.js
@@ -20,45 +20,46 @@ class Colorbox extends Component{
   constructor(props) {
     super(props)
     this.state = {
-      count: 0,
+      count: 0,                     // Sets initial count to 0
       box: <p 
-        style={{
-        background: "white", 
-        border: '2px solid black', 
-        width: '150px'
+        style={{                    // Sets inital box on load to:
+        background: "white",        // White Background
+        border: '2px solid black',  // Solid Black Border
+        width: '150px'              // 150px Wide
         }}
         >White</p>
     };
   };
 
   changeColor = () => {
+    //available colors are in array
     var colorArr = ["green", "blue", "yellow", "red", "purple", "orange"];
     let newCount = this.state.count
 
-    if (this.state.count < 5) {
-      newCount = this.state.count + 1
-    } else {
-      newCount = 0;
+    if (this.state.count < 5) {       // If count is less than 5,
+      newCount = this.state.count + 1 // add state.count to newCount
+    } else {                          // else 
+      newCount = 0;                   // Reset newCount back to 0 to start over
     }
 
-    let color = colorArr[newCount];
-    this.setState({ 
+    let color = colorArr[newCount];   // color to be used is based on iteration of colorArr[] array
+    this.setState({                   // changes state and sets background color
       box: <p style={{
         background: color,
         border: '2px solid black', 
         width: '150px'
-        }}>{color}</p>, 
-      count: newCount
-    }); // {color: newColor})
+        }}>{color}</p>,               // Displays color name in box
+      count: newCount                 // Saves new count in this.state.count
+    });
   };
 
   render() {
     var {box} = this.state
     var {count} = this.state
-    return(
+    return( // Displays box with background color // calls changeColor() to change {box} color
       <>
-      <h1>{box}</h1>
-      <button onClick = {this.changeColor}>Press me!</button>
+      <h1>{box}</h1>                  
+      <button onClick = {this.changeColor}>Press me!</button>   
       </>
     )
   };
