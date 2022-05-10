e18f48352b1238c7f6a5a09c56b7cf93829dffe5
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-09 22:17:50 -0400

    added carInfo() method to class

--- javascript-foundations-challenges/inheritance-jeremy.js
+++ javascript-foundations-challenges/inheritance-jeremy.js
@@ -7,19 +7,27 @@
 // - The year for the car class can be "myCar year"
 // 4. **Story**:	As a programmer, I can tell how many wheels myCar has.
 // - Calling the method wheels will return 4
-
 // 13. **Story**: As a programmer, I can give all my cars a lights property. Lights start in the off position.
 // 14. **Story**: As a programmer, I can turn the lights in all my cars on and off.
 // 15. **Story**:  As a programmer, I can give all my cars a signal property. Turn signal starts in the off position.
 // 16. **Story**:	As a programmer, I can determine the speed of a car. Speed starts at 0 mph.
+// 23. **Story**:  As a programmer, I can call upon a carInfo method that will tell me all the information about a car.
+// - The method can be created in the parent class and accessed by all child classes
   class Car { 
-    constructor(year, model){
+    constructor(year, model="generic model"){
       this.wheels = 4
+      this.make = "generic make"
       this.model = model
       this.year = year
-      this.rpm = 0
       this.lights = "Off"
       this.signal = "Off" 
+      this.speed = 0
+    }
+    carInfo() {
+      console.log(`${this.year} \n${this.make} \n${this.model} \n${this.wheels} Wheels \nCar Lights = ${this.lights} \nCar Signal = ${this.signal} \nCar Speed = ${this.speed} MPH`);
+    }
+    determineSpeed() {
+      console.log(`The current speed is ${this.speed} MPH.`)
     }
     turnLeftSignal() {
       this.signal = "Left";
@@ -40,13 +48,13 @@
       }
     }
     accelerate() {
-      this.rpm += 250
+      this.speed += 5;
     }
     decelerate() {
-      if (this.rpm > 0) {
-        this.rpm -= 250
+      if (this.speed > 0) {
+        this.speed -= 5;
       }else {
-        this.rpm = 0
+        this.speed = 0;
       }
     }
     getWheelCount() {
@@ -54,9 +62,10 @@
     }
   }
 
-  var myCar = new Car(2012, "generic car");
+  var myCar = new Car(2012);
   console.log(myCar);
   myCar.getWheelCount();
+
 
 // 4. **Story**:	As a programmer, I can make a Tesla car.
 // - class Tesla inherits from class Car
@@ -65,14 +74,33 @@
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 // 6. **Story**: As a programmer, I can give my Tesla a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+// 17. **Story**:	As a programmer, I can speed my Tesla up by 10 per acceleration.
+
+// 18. **Story**:	As a programmer, I can slow my Tesla down by 7 per braking.
   class Tesla extends Car{
     constructor(year, model){
-      super(year, model)
+      super(year, model) 
+    }
+    make = "Tesla"
+    accelerate() {
+      this.speed += 10;
+    }
+    decelerate() {
+      if (this.speed > 0) {
+        this.speed -= 7;
+        if(this.speed < 0) {
+          this.speed = 0;
+        }
+      }else {
+        this.speed = 0;
+      }
     }
   }
 
   var myTesla = new Tesla(2022, "Plaid");
   console.log(myTesla);
+
 // 7. **Story**:	As a programmer, I can make a Toyota car.
 // - class Toyota inherits from class Car
 // - create an object called myToyota which is a instance of class Toyota
@@ -80,16 +108,20 @@
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 // 9. **Story**: As a programmer, I can give my Toyota a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+// 19. **Story**:	As a programmer, I can speed my Toyota up by 5 per acceleration.
+
+// 20. **Story**:	As a programmer, I can slow my Toyota down by 2 per braking.
   class Toyota extends Car{
     constructor(year, model){
       super(year, model)
       this.horsepower = 380
     }
+    make = "Toyota"
   }
 
   var myToyota = new Toyota(2021, "Supra");
   console.log(myToyota);
-
 
 
 // 10. **Story**:	As a programmer, I can make a Volkswagen car.
@@ -99,10 +131,15 @@
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 // 12. **Story**: As a programmer, I can give my Volkswagen a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+// 21. **Story**:	As a programmer, I can speed my Volkswagen up by 7 per acceleration.
+
+// 22. **Story**:	As a programmer, I can slow my Volkswagen down by 5 per braking.
   class Volkswagen extends Car{
     constructor(year, model){
       super(year, model)
     }
+    make = "Volkswagon"
   }
 
   var myVolkswagen = new Volkswagen(2015, "Beetle");
@@ -111,17 +148,9 @@
 
 
 
-// 17. **Story**:	As a programmer, I can speed my Tesla up by 10 per acceleration.
 
-// 18. **Story**:	As a programmer, I can slow my Tesla down by 7 per braking.
 
-// 19. **Story**:	As a programmer, I can speed my Toyota up by 5 per acceleration.
 
-// 20. **Story**:	As a programmer, I can slow my Toyota down by 2 per braking.
 
-// 21. **Story**:	As a programmer, I can speed my Volkswagen up by 7 per acceleration.
 
-// 22. **Story**:	As a programmer, I can slow my Volkswagen down by 5 per braking.
 
-// 23. **Story**:  As a programmer, I can call upon a carInfo method that will tell me all the information about a car.
-// - The method can be created in the parent class and accessed by all child classes
