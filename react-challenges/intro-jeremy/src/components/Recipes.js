029603329265811371585da36434ab279d9c9a86
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-10 09:21:51 -0400

    Created a Recipes component.

---
+++ react-challenges/intro-jeremy/src/components/Recipes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Recipes extends Component{
+  render(){
+    return(
+      <>
+        <h1>This is the recipes</h1>
+      </>
+    )
+  }
+}
+
+export default Recipes
