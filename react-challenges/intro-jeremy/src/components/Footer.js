dccc5916a0f7ad3efa3ecc5fcfc604710c7e980f
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-10 09:28:42 -0400

    Created Footer component.

---
+++ react-challenges/intro-jeremy/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+        LEARN Academy
+        <br />
+        Delta 2022
+      </>
+    )
+  }
+}
+
+export default Footer
