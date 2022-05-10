28c6ac2dd837a27c9289cff38c67b91d6b7e9e5d
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-09 23:16:08 -0400

    Completed challenges

--- javascript-foundations-challenges/spread-operator-jeremy.js
+++ javascript-foundations-challenges/spread-operator-jeremy.js
@@ -14,12 +14,26 @@ console.log(combinedArr);
 
 // #### Exercise 2
 // Consider this function:
-function combineAndFilterOdd(arrOne, arrTwo, arrThree){
-  return arrOne.concat(arrTwo).concat(arrThree).filter(num => num % 2 !== 0)
-}
+  function combineAndFilterOdd(arrOne, arrTwo, arrThree){
+    return arrOne.concat(arrTwo).concat(arrThree).filter(num => num % 2 !== 0);
+  }
+  console.log(combineAndFilterOdd([3, 2, 5], [5, 8, 7], [4, 5, 6]));
 
-console.log(combineAndFilterOdd([3, 2, 5], [5, 8, 7], [4, 5, 6]))
 // Refactor this function to use the spread operator to combine the array arguments.
+var arr1 = [3, 2, 5]; var arr2 =  [5, 8, 7]; var arr3 = [4, 5, 6];
+var combinedArr = [...arr1, ...arr2, ...arr3]; 
+console.log(combinedArr.filter(num => num % 2 !== 0));
 
 // **STRETCH**
 // Recall the **combineAndFilterOdd()** function from the previous exercise. Refactor the function to take any number of arguments.
+function combineAndFilterOdd(...arr){
+  var combined = [...arr];
+  var result = [];
+  for (var i = 0; i < combined.length; i++) {
+    for (var j = 0; j < combined[i].length; j++) {
+      result.push(combined[i][j])
+    }
+  }
+  return result.filter(num => num % 2 !== 0);
+}
+console.log(combineAndFilterOdd([3, 2, 5], [5, 8, 7], [4, 5, 6]));
