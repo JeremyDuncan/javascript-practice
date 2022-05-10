210a97e3f1fc1334bfd57a50bd99d78ae0b61fa2
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-10 10:01:07 -0400

    Added my name

--- react-challenges/intro-jeremy/src/components/Profile.js
+++ react-challenges/intro-jeremy/src/components/Profile.js
@@ -5,7 +5,8 @@ class Profile extends Component{
   render(){
     return(
       <>
-      <h2>About Me:</h2>
+      <h2>Jeremy Duncan</h2>
+      <h3>About Me:</h3>
       <p>I am a veteran that decided to make a career change from being an Electronic Technician and Network Administrator in the Navy.</p>
       <p>I am dedicated and determined to gain the knowledge necessary to be a great asset to any company that would like a hard working software engineer on their team. </p>
       </>
