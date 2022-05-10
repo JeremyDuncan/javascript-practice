0ba0f11342f673feb8e3f59e914bbccfb63f762c
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-10 09:57:29 -0400

    Updated Profile component.

--- react-challenges/intro-jeremy/src/components/Profile.js
+++ react-challenges/intro-jeremy/src/components/Profile.js
@@ -1,1 +1,16 @@
 //- As a user, I can see a Profile component with information about you and your partner
+import React, { Component } from 'react'
+
+class Profile extends Component{
+  render(){
+    return(
+      <>
+      <h2>About Me:</h2>
+      <p>I am a veteran that decided to make a career change from being an Electronic Technician and Network Administrator in the Navy.</p>
+      <p>I am dedicated and determined to gain the knowledge necessary to be a great asset to any company that would like a hard working software engineer on their team. </p>
+      </>
+    )
+  }
+}
+
+export default Profile
