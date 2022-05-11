5895d7ac78eb28eb18b175507e2c45b40b8878e8
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-11 19:51:46 -0400

    Created Roll.js and Dice.js. Sttarted the boilerplate for the modules.

---
+++ react-challenges/props-jeremy/src/Components/Roll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Roll extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Roll Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Roll
