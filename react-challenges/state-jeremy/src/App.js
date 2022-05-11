d19fcffc6eaad0c38bc7474a0cee45a7ab904c41
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-11 19:41:23 -0400

    Completed all challenges including stretch challenges. Added comment to code for easier understanding of logic.

--- react-challenges/state-jeremy/src/App.js
+++ react-challenges/state-jeremy/src/App.js
@@ -7,23 +7,22 @@
 // - As a user, I can cycle through a list of color names, one per click - green, blue, yellow, red, purple, orange
 // - As a user, when I click the box, the background changes to match the name of the color name displayed in the box
 // - As a user, I can see many color boxes on the page all acting independently of one another
-
 // #### Stretch
-
 // - As a user, I can see a button to add or remove color boxes
 // - As a user, when I click the add button, I can add one colorbox that acts independently of the other colorboxes
 // - As a user, when I click the remove button, I can remove one colorbox
+
 import React, { Component } from 'react'
 import Colorbox from './components/Colorbox'
 class App extends Component {
   constructor(props) {
     super(props) 
     this.state = {
-      boxes: [
-      ],
-      currentBoxes: -1
+      boxes: [], //Empty array to hold <Colorbox/> Modules
+      currentBoxes: -1 // Keep count of button preses and used to splice during removeBox()
     }
   }
+
   // Adds new <Colorbox/> Module to this.state.boxes[] Array for every call 
   addBox = () => {
    var  box = <Colorbox/>
@@ -32,21 +31,16 @@ class App extends Component {
 
   // Removes 1 <Colorbox/> Module from this.state.boxes[] Array for every call 
   removeBox = () => {
-    var box = <colorbox/>
     var i = this.state.currentBoxes
     this.setState({boxes: [...this.state.boxes.slice(0,i), ...this.state.boxes.slice(i+1)], currentBoxes: this.state.currentBoxes - 1 });
   }
 
-  
-  
   render() {
     {var count = this.state.currentBoxes}
     return (
-      <>
+      <div>
         { this.state.boxes }
-
         <br/><br/>
-
         {/* Button calls addBox() */}
         {/* Adds 1 <Colorbox/> Component per click */}
         <button onClick={this.addBox}>                        
@@ -58,13 +52,9 @@ class App extends Component {
         <button onClick={count > -1 ? this.removeBox: null}>  
           - Remove Box                                        
         </button>                   
-
-        {console.log(this.state.currentBoxes)}
-        {console.log(this.state.boxes)}
-      </>
+      </div>
     );
   }
-  }
+}
  
-
 export default App;
