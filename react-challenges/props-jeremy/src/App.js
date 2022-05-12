e4c536409f03ca55ff932250f684f9d7b962eb7b
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-11 20:14:27 -0400

    Added button for Rolls. Added components to App.js

--- react-challenges/props-jeremy/src/App.js
+++ react-challenges/props-jeremy/src/App.js
@@ -4,11 +4,11 @@
 
 // ![dice game](./assets/dice-game.png)
 
-// - As a user, I can see an application called Dice Roller
-//   - As a developer, I can create a React application with `App.js` as my stateful component
- //  - As a developer, I can create two child components that will accept props from `App.js`
-// - As a user, I can click a box and see the outcome of my current "roll"
-//   - As a developer, I can pass a method from `App.js` to my dice component to display a number between 1 and 6
+//! - As a user, I can see an application called Dice Roller 
+//  - As a developer, I can create a React application with `App.js` as my stateful component
+//  - As a developer, I can create two child components that will accept props from `App.js`
+//  - As a user, I can click a box and see the outcome of my current "roll"
+//  - As a developer, I can pass a method from `App.js` to my dice component to display a number between 1 and 6
 // - As a user, I can see my roll logged
 //   - As a developer, I can pass the value of the roll to a log component
 // - As a user, I can see the roll log continue to grow as I roll the dice
@@ -20,11 +20,19 @@
 import React, { Component } from 'react'
 import Dice from './Components/Dice'
 import Roll from './Components/Roll'
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+     
+    }
+  }
   render(){
     return(
       <div>
-        <h1>Dice Roll App</h1>
+        <h1>Dice Roller!</h1>
+        <Roll/>
+        <Dice/>
       </div>
     )
   }
--- react-challenges/props-jeremy/src/Components/Roll.js
+++ react-challenges/props-jeremy/src/Components/Roll.js
@@ -1,10 +1,40 @@
+//  - As a developer, I can create two child components that will accept props from `App.js`
+//  - As a user, I can click a box and see the outcome of my current "roll"
+//  - As a developer, I can pass a method from `App.js` to my dice component to display a number between 1 and 6
+// - As a user, I can see my roll logged
+//   - As a developer, I can pass the value of the roll to a log component
+// - As a user, I can see the roll log continue to grow as I roll the dice
+
+// ### Stretch Goals
+// - As a user, I can see the image of a dice face when I "roll" the dice
+// - As a user, I can click a restart button that clears my roll log
+
+
 import React, { Component } from 'react'
 
 class Roll extends Component{
+  constructor(props) {
+    super(props) 
+    this.state = {
+      diceResult: 0
+    }
+  }
+  
+  rollDice = () => {
+
+    this.setState({diceResult: 3 })
+  }
+  
   render(){
+    
+    {console.log(this.state.diceResult)}
+
     return(
       <div>
         <h1>Roll Component</h1>
+        <button onClick={this.rollDice}>
+          Roll Dice!
+        </button>
       </div>
     )
   }
