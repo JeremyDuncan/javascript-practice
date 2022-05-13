5cb6e36191ae050dd36bd37e24e51e8bf702ebf2
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-13 09:44:42 -0400

    Added food images to images folder. Created Image.js to import images.

---
+++ react-challenges/funct-props-jeremy/src/components/Images.js
@@ -0,0 +1,16 @@
+import Pizza from '../images/Pizza.jpg'
+import Hamburger from '../images/Hamburger.jpg'
+import Taco from '../images/Taco.jpg'
+import Curry from '../images/Curry.jpg'
+import Steak from '../images/Steak.jpg'
+
+// images from ./images folder stored into object
+const foodImage = {
+  Pizza,
+  Hamburger,
+  Taco,
+  Curry,
+  Steak
+}
+
+export default foodImage
