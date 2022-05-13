481ae5fb056480a695560b54ba928aac108858ae
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-13 08:11:11 -0400

    Added require statement to import picture of pizza

--- react-challenges/funct-props-jeremy/src/components/SelectedFood.js
+++ react-challenges/funct-props-jeremy/src/components/SelectedFood.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 class SelectedFood extends Component {
-
   render(){
     return(
       <div>
-        <div className="foodlist">
-            <h1>Selected Food</h1>
-          <ul>
-            {this.props.choices.map((choice, index) => {
-              return <li key={index}>{choice}</li>
-              })}
-          </ul>
-        </div>
-
+        <h1>Selected Food</h1>
+        <ul>
+          {this.props.choices.map((choice, index) => {
+            return <li key={index}>
+              {choice}
+              <img src={require('../images/Pizza.jpg')}height={50} width={50}/>
+              </li>
+            })}
+        </ul>
       </div>
     )
   }
 }
-
-
-
-
 
 export default SelectedFood
