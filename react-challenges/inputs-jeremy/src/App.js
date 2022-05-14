f36cc46b1d3ee77b5dcc82be8b774a7c76cd6691
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-14 09:22:37 -0400

    Added onChange(), and handleChange() for user input

--- react-challenges/inputs-jeremy/src/App.js
+++ react-challenges/inputs-jeremy/src/App.js
@@ -4,9 +4,9 @@
         // As a user, I can see a page with a text input.
         // As a developer, I have one parent component that holds state (logic or smart component).
         // As a developer, I have an input in my parent component.
-// As a user, I see titles of three robots waiting for my text.
-    // As a developer, I have three child components that do not hold state (display or dumb component).
-// As a user, when I enter text I see the three robots responses update in real time.
+        // As a user, I see titles of three robots waiting for my text.
+        // As a developer, I have three child components that do not hold state (display or dumb component).
+        // As a user, when I enter text I see the three robots responses update in real time.
     // As a developer, I can call an onChange method on the input tag.
     // As a developer, I can pass the updated state as props to the child components.
     // As a developer, I can see the child components display the user input.
@@ -30,12 +30,19 @@ class App extends Component {
     }
   }
 
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({userInput: e.target.value})
+  }
+
   render(){
     return(
-      <div>
-          <input type="text" value={this.state.userInput}/>
+      <div className="App">
+          <input type="text" value={this.state.userInput} onChange={this.handleChange}/>
           <GoodRobot userInput={this.state.userInput}/>
+          <br/>
           <BadRobot userInput={this.state.userInput}/>
+          <br/>
           <KanyeRobot userInput={this.state.userInput}/>
       </div>
     )
