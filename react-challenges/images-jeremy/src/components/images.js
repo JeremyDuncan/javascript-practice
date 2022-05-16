7eb1e2d95c567813fb9c879258c41ed97dcd6f80
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-16 13:07:56 -0400

    updated variables for images.js

---
+++ react-challenges/images-jeremy/src/components/images.js
@@ -0,0 +1,10 @@
+import On from '../images/switch-on.png'
+import Off from '../images/switch-off.png'
+
+// images from ./images folder stored into object
+const switchImage = {
+  On,
+  Off
+}
+
+export default switchImage
