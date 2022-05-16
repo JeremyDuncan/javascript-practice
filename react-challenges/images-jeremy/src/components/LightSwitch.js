931218c5d1db9a7f47f9e6f3821a1ff657b84d1b
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-15 21:43:38 -0400

    Added boilerplate to LightSwitch.js

---
+++ react-challenges/images-jeremy/src/components/LightSwitch.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class LightSwitch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>START HERE</h1>
+
+      </div>
+    )
+  }
+}
+export default LightSwitch;
